8908ae0a2afc9f024c9629124f827e3b5912f93d
Cedrouse Roll <omondicedo@gmail.com>
2021-09-29 22:12:33 +0300

    Update legacy.js

--- rollindustries/static/rollindustries/js/legacy.js
+++ rollindustries/static/rollindustries/js/legacy.js
@@ -129,7 +129,7 @@ navSlide();
      for (let a = 0; a < particlesArray.length; a++) {
          for (let b = a; b < particlesArray.length; b++) {
              let distance = (( particlesArray[a].x -particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x)) + ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y));
-             if (distance < (canvas.width/12) * (canvas.height/12)) {
+             if (distance < (canvas.width/10) * (canvas.height/1o)) {
                  ctx.strokeStyle='#7fffd4';
                  ctx.lineWidth = 1;
                  ctx.beginPath();
