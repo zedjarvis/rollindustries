ca0d14f78f24deb8071955ab0dcb1f26bef58c1d
Cedrouse Roll <omondicedo@gmail.com>
2021-09-29 14:35:58 +0300

    reduced repo to only rollindustries mainpage

--- MyWebsite/urls.py
+++ MyWebsite/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rollindustries.urls')),
-    path('accounts/', include('accounts.urls')),
 ]
 
 
